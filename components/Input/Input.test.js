e10b6de5ad388373e113526dda84e2fcbbe0a6da
Thomas Kirst <thomas.kirst@neuefische.de>
2022-12-20 23:35:28 +0100

    Add possible solution

--- components/Input/Input.test.js
+++ components/Input/Input.test.js
@@ -2,6 +2,28 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+  render(<Input labelText="Name of game" placeholder="Dodelido" name="game" />);
 
-test("calls callback on every user input", async () => {});
+  const inputField = screen.getByLabelText("Name of game");
+  expect(inputField).toBeInTheDocument();
+  expect(inputField).toHaveAttribute("placeholder", "Dodelido");
+});
+
+test("calls callback on every user input", async () => {
+  const mockFunction = jest.fn();
+  render(
+    <Input
+      labelText="Name of game"
+      placeholder="Dodelido"
+      name="game"
+      onChange={mockFunction}
+    />
+  );
+  const inputField = screen.getByLabelText("Name of game");
+
+  const user = userEvent.setup();
+  await user.type(inputField, "abc");
+
+  expect(mockFunction).toHaveBeenCalledTimes(3);
+});
